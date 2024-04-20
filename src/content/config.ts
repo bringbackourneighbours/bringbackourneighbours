c729606db351023da63823473fcbdd06f6de1e9f
Johannes Voigt <10059188+jvoigt@users.noreply.github.com>
2024-04-20 12:02:55 +0200

    use content collection for a flyer with custom i18n

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,30 @@
 import { z, defineCollection } from 'astro:content';
 
+const i18nUrlSchema = {
+  title: z.string(),
+  identifier: z.string(),
+  lang: z.string(),
+  seo: z.string(),
+  fallback: z.string().optional(),
+};
+
 const pagesCollection = defineCollection({
   type: 'content',
   schema: z.object({
-    title: z.string(),
-    identifier: z.string(),
-    lang: z.string(),
-    seo: z.string(),
-    fallback: z.string().optional(),
+    ...i18nUrlSchema,
+  }),
+});
+
+const flyersCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    ...i18nUrlSchema,
+    germanTitle: z.string(),
+    lastChecked: z.date(),
   }),
 });
 
 export const collections = {
   pages: pagesCollection,
+  flyers: flyersCollection,
 };
