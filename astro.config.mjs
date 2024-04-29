acc98ab29994594df0a14675915a913cfaac1450
ponychicken <ponychicken@users.noreply.github.com>
2024-04-29 14:06:03 +0200

    Use environment vars when building site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,11 @@ import mdx from "@astrojs/mdx";
 const isDev = import.meta.env.DEV;
 
 const siteUrl = isDev? 'http://localhost:4321' : 'https://bringbackourneighbours.github.io';
-const base = isDev? '/' : '/bringbackourneighbours';
+const basePath = isDev? '/' : '/bringbackourneighbours';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://localhost:4321',
+  site: siteUrl,
+  base: basePath,
   integrations: [mdx()]
 });
