d0983d5acee0f6f8cd979c2955b129afa60a50a0
Johannes Voigt <10059188+jvoigt@users.noreply.github.com>
2024-05-12 16:38:47 +0200

    feat: add redirect for link as example

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
-
 const isDev = import.meta.env.DEV;
-
-const siteUrl = isDev? 'http://localhost:4321/' : 'https://bringbackourneighbours.github.io/bringbackourneighbours/';
-const basePath = isDev? '/' : '/bringbackourneighbours';
+const siteUrl = isDev ? 'http://localhost:4321/' : 'https://bringbackourneighbours.github.io/bringbackourneighbours/';
+const basePath = isDev ? '/' : '/bringbackourneighbours';
 
 // https://astro.build/config
 export default defineConfig({
   site: siteUrl,
   base: basePath,
-  integrations: [mdx()]
+  integrations: [mdx()],
 });
