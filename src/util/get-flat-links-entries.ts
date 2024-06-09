08fa6d80ac982b8a5099ba0b723c51ab4c8cd92a
Johannes Voigt <10059188+jvoigt@users.noreply.github.com>
2024-06-09 20:18:26 +0200

    feat: redirect to internal kit and flyer via link/slug

--- src/util/get-flat-links-entries.ts
+++ src/util/get-flat-links-entries.ts
@@ -1,15 +1,59 @@
 import { getCollection, type CollectionEntry } from 'astro:content';
+import {
+  getStaticPathsForFlyers,
+  getStaticPathsForKits,
+} from './get-static-paths.ts';
+import {
+  getCanonicalUrlToFlyer,
+  getCanonicalUrlToKit,
+} from './get-canonical-url.ts';
 
 export interface LinkData {
   slug: string;
   title: string;
-  url: string;
+  url?: string;
   type: string;
 }
 
 export const getFlatLinksEntries = async (): Promise<LinkData[]> => {
+  return [
+    ...(await getAllExternalLinksEntries()),
+    ...(await getAllFlyerLinksEntries()),
+    ...(await getAllKitLinksEntries()),
+  ];
+};
+
+const getAllExternalLinksEntries = async (): Promise<LinkData[]> => {
   const linkEntries: CollectionEntry<'links'>[] = await getCollection('links');
   return linkEntries.reduce((accumulator, currentValue) => {
     return [...accumulator, ...Object.values(currentValue.data).flat()];
   }, [] as LinkData[]);
+};
+
+const getAllFlyerLinksEntries = async (): Promise<LinkData[]> => {
+  return Promise.all(
+    (await getStaticPathsForFlyers()).map(async (flyer) => {
+      const data = flyer.props.entry.data;
+      return {
+        slug: `flyer-${data.lang}-${data.identifier}`,
+        title: data.title,
+        type: 'FLYER',
+        url: await getCanonicalUrlToFlyer(data.lang, data.identifier),
+      };
+    }),
+  );
+};
+
+const getAllKitLinksEntries = async (): Promise<LinkData[]> => {
+  return Promise.all(
+    (await getStaticPathsForKits()).map(async (kit) => {
+      const data = kit.props.entry.data;
+      return {
+        slug: `kit-${data.lang}-${data.identifier}`,
+        title: data.title,
+        type: 'KIT',
+        url: await getCanonicalUrlToKit(data.lang, data.identifier),
+      };
+    }),
+  );
 };
