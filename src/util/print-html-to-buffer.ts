b9ebcb750cbdf777b5369d8ccfa74d30ffcada1c
Johannes Voigt <10059188+jvoigt@users.noreply.github.com>
2024-06-16 15:25:33 +0200

    feat: styling for print

--- src/util/print-html-to-buffer.ts
+++ src/util/print-html-to-buffer.ts
@@ -5,6 +5,9 @@ export const printHtmlToBuffer = async (distPath: string): Promise<Buffer> => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
+  // we will not get the global stylesheets without loading them first.
+  // so we visit the porduction page once and then the css is already loaded
+  // alternatively we could intercept the request for the styles... do it works for now.
   await page.goto(
     'https://bringbackourneighbours.github.io/bringbackourneighbours/',
   );
@@ -12,20 +15,10 @@ export const printHtmlToBuffer = async (distPath: string): Promise<Buffer> => {
   const contentHtml = fs.readFileSync(distPath, 'utf8');
   await page.setContent(contentHtml, { waitUntil: 'domcontentloaded' });
 
-  const pdfBuffer = await page.pdf();
-
-  await browser.close();
-
-  return pdfBuffer;
-};
-
-export const prinUrlToBuffer = async (url: string): Promise<Buffer> => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-
-  await page.goto(url);
-
-  const pdfBuffer = await page.pdf();
+  const pdfBuffer = await page.pdf({
+    printBackground: true,
+    preferCSSPageSize: true,
+  });
 
   await browser.close();
 
