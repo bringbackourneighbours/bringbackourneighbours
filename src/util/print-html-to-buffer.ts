1f75b967564323cad7b24d6f2a9fd9d708e02e59
Johannes Voigt <10059188+jvoigt@users.noreply.github.com>
2024-06-16 13:39:35 +0200

    chore: disable integration

--- src/util/print-html-to-buffer.ts
+++ src/util/print-html-to-buffer.ts
@@ -5,10 +5,25 @@ export const printHtmlToBuffer = async (distPath: string): Promise<Buffer> => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  await page.goto('https://example.com');
+  await page.goto(
+    'https://bringbackourneighbours.github.io/bringbackourneighbours/',
+  );
 
   const contentHtml = fs.readFileSync(distPath, 'utf8');
   await page.setContent(contentHtml, { waitUntil: 'domcontentloaded' });
+
+  const pdfBuffer = await page.pdf();
+
+  await browser.close();
+
+  return pdfBuffer;
+};
+
+export const prinUrlToBuffer = async (url: string): Promise<Buffer> => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto(url);
 
   const pdfBuffer = await page.pdf();
 
