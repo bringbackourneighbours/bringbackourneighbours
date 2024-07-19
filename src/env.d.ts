f1007d770213bd9c9b0049e5e94c36fbb3a5c142
Johannes Voigt <10059188+jvoigt@users.noreply.github.com>
2024-07-19 10:43:43 +0200

    feat: set language as Astro local to avoid repeating it again and again

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,8 +6,12 @@
 declare namespace App {
   interface Locals {
     /**
-     * when set then we ari trying to print thi current page: so please try to look printlike
+     * when set then we are trying to print this current page: so please try to look printlike
      */
     isPrint: boolean;
+    /**
+     * the current context is in this language: all components can use this if not set explicitly
+     */
+    lang: import('./util/languages.ts').LanguagesValue;
   }
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,14 @@
 import { defineMiddleware } from 'astro:middleware';
+import { DEFAULT_LANG, type LanguagesValue } from './util/languages.ts';
 
 // `context` and `next` are automatically typed
 export const onRequest = defineMiddleware((context, next) => {
   // Components can check if isPrint and show things differently
   context.locals.isPrint = context.url.pathname.startsWith('/internal-print/');
+
+  // Read Lang (or default) from params
+  context.locals.lang =
+    (context.params['lang'] as LanguagesValue) || DEFAULT_LANG;
 
   return next();
 });
