443f476ce0626e5fa71df9938bafd2ab435b8fde
Johannes Voigt <10059188+jvoigt@users.noreply.github.com>
2024-09-08 12:22:37 +0200

    fix: dont crash on h1 in toc

--- src/util/table-of-contents.ts
+++ src/util/table-of-contents.ts
@@ -10,8 +10,11 @@ export function buildToc(headings: MarkdownHeading[]): NestedMarkdownHeading[] {
   headings.forEach((h) => {
     const heading = { ...h, subheadings: [] };
     parentHeadings.set(heading.depth, heading);
-    // our markdown should not include <h1>... so we ues h2 as top node
-    if (heading.depth === 2) {
+    // our markdown should not include <h1>...
+    if (heading.depth === 1) {
+      return;
+    } else if (heading.depth === 2) {
+      // so we ues h2 as top node
       toc.push(heading);
     } else {
       parentHeadings.get(heading.depth - 1).subheadings.push(heading);
