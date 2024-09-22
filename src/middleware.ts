8e3cfa7970710958b948b0319409f3b2a4837f6f
Johannes Voigt <10059188+jvoigt@users.noreply.github.com>
2024-09-22 17:07:32 +0200

    feat: handle ShortLink in print for flyer and kits

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
 import { defineMiddleware } from 'astro:middleware';
+
 import { DEFAULT_LANG, type LanguagesValue } from './util/languages.ts';
 import AstroConfig from '../astro.config.mjs';
 
@@ -8,6 +9,20 @@ export const onRequest = defineMiddleware((context, next) => {
   context.locals.isPrint = context.url.pathname.startsWith(
     `${AstroConfig.base}/internal-print/`,
   );
+
+  context.locals.isKit =
+    context.url.pathname.startsWith(`${AstroConfig.base}/internal-print/kit`) ||
+    context.url.pathname.startsWith(
+      `${AstroConfig.base}/${context.params['lang']}/kit/`,
+    );
+
+  context.locals.isFlyer =
+    context.url.pathname.startsWith(
+      `${AstroConfig.base}/internal-print/flyer`,
+    ) ||
+    context.url.pathname.startsWith(
+      `${AstroConfig.base}/${context.params['lang']}/flyer/`,
+    );
 
   // Read Lang (or default) from params
   context.locals.lang =
