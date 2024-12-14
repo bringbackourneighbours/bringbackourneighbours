b77eac7ddda2b13961e7183e677091e3d0ac2537
Johannes Voigt <10059188+jvoigt@users.noreply.github.com>
2024-12-14 15:34:24 +0100

    chore(#127): check the page count of all flyers with muhammara as integration

---
+++ src/integrations/check-flyers.ts
@@ -0,0 +1,35 @@
+import { type AstroIntegration } from 'astro';
+import { readdir } from 'node:fs/promises';
+import muhammara from 'muhammara';
+
+export default function checkFlyers(): AstroIntegration {
+  return {
+    name: 'check-flyers',
+    hooks: {
+      'astro:build:done': async ({ dir, logger }): Promise<void> => {
+        logger.info('Checking each flyer for the right page count');
+
+        const pdfDistDir = `${dir.pathname}print`;
+        const pdfsFileNames = await readdir(pdfDistDir);
+
+        pdfsFileNames
+          .filter((fileName) => {
+            logger.debug(`Checking if ${fileName} is a flyer`);
+            return fileName.startsWith('flyer');
+          })
+          .forEach((fileName) => {
+            const pdfReader = muhammara.createReader(
+              `${pdfDistDir}/${fileName}`,
+            );
+            const pagesCount = pdfReader.getPagesCount();
+
+            if (pagesCount > 4) {
+              logger.error(`${fileName}: ${pagesCount} pages`);
+            } else {
+              logger.debug(`${fileName}: ${pagesCount} pages`);
+            }
+          });
+      },
+    },
+  };
+}
