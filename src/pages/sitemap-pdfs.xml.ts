d1accc7baf8fe2e86c9e7a80a6595ee14826141b
Johannes Voigt <10059188+jvoigt@users.noreply.github.com>
2025-05-30 17:01:58 +0200

    feat: generate sitemap for pdfs

---
+++ src/pages/sitemap-pdfs.xml.ts
@@ -0,0 +1,57 @@
+import type { APIRoute } from 'astro';
+import {
+  getStaticPaths,
+  type StandaloneCollections,
+} from '../util/get-static-paths.ts';
+import { getCanonicalUrlFn } from '../util/get-canonical-url.ts';
+import { getTranslationsUrls } from '../util/get-translations-url.ts';
+import { renderSitemapUrlset, type SiteMapUrl } from '../util/sitemap.ts';
+import { getPrintUrl } from '../util/get-absolute-url.ts';
+
+async function getSiteMapUrls(
+  collection: StandaloneCollections,
+): Promise<SiteMapUrl[]> {
+  const pages = await getStaticPaths(collection);
+  return await Promise.all(
+    pages.map(async (entry): Promise<SiteMapUrl> => {
+      const lang = entry.params.lang;
+      const identifier = entry.params.identifier;
+      const translations = await getTranslationsUrls(
+        collection,
+        lang,
+        identifier,
+        getCanonicalUrlFn(collection),
+      );
+      const url = getPrintUrl(collection, lang, identifier);
+
+      return {
+        loc: new URL(url),
+        lastMod: entry.props.entry.data.lastChecked,
+        links: [
+          {
+            href: new URL(url!),
+            hreflang: lang,
+          },
+          ...translations.map((translation) => ({
+            hreflang: translation.lang as string,
+            href: new URL(
+              getPrintUrl(collection, translation.lang, identifier),
+            ),
+          })),
+        ],
+      };
+    }),
+  );
+}
+
+export const GET: APIRoute = async () => {
+  const kits: SiteMapUrl[] = await getSiteMapUrls('kits');
+  const flyers: SiteMapUrl[] = await getSiteMapUrls('flyers');
+
+  return new Response(renderSitemapUrlset([...kits, ...flyers]), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  });
+};
--- src/util/get-absolute-url.ts
+++ src/util/get-absolute-url.ts
@@ -1,5 +1,16 @@
 import AstroConfig from '../../astro.config.mjs';
+import type { StandaloneCollections } from './get-static-paths.ts';
 
 export const getAbsoluteUrl = (path: string): string => {
   return `${AstroConfig.site}${path}`;
 };
+
+export function getPrintUrl(
+  collection: StandaloneCollections,
+  lang: string,
+  identifier: string,
+) {
+  return getAbsoluteUrl(
+    `print/${collection.slice(0, -1)}-${lang}-${identifier}.pdf`,
+  );
+}
