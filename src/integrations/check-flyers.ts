aac520006df0de3cc45736840143cf4e40a9a566
Johannes Voigt <10059188+jvoigt@users.noreply.github.com>
2025-06-14 16:10:30 +0200

    feat(#694): replace muhammara with better maintained pdf-lib

--- src/integrations/check-flyers.ts
+++ src/integrations/check-flyers.ts
@@ -1,6 +1,7 @@
 import { type AstroIntegration } from 'astro';
 import { readdir } from 'node:fs/promises';
-import muhammara from 'muhammara';
+import fs from 'fs';
+import { PDFDocument } from 'pdf-lib';
 
 export default function checkFlyers(): AstroIntegration {
   return {
@@ -12,23 +13,30 @@ export default function checkFlyers(): AstroIntegration {
         const pdfDistDir = `${dir.pathname}print`;
         const pdfsFileNames = await readdir(pdfDistDir);
 
-        pdfsFileNames
-          .filter((fileName) => {
-            logger.debug(`Checking if ${fileName} is a flyer`);
-            return fileName.startsWith('flyer');
-          })
-          .forEach((fileName) => {
-            const pdfReader = muhammara.createReader(
-              `${pdfDistDir}/${fileName}`,
-            );
-            const pagesCount = pdfReader.getPagesCount();
+        let hasOverprinting = false;
 
-            if (pagesCount > 4) {
-              logger.error(`${fileName}: ${pagesCount} pages`);
-            } else {
-              logger.info(`${fileName}: ${pagesCount} pages`);
-            }
-          });
+        for (const fileName1 of pdfsFileNames.filter((fileName) => {
+          logger.debug(`MUHA Checking if ${fileName} is a flyer`);
+          return fileName.startsWith('flyer');
+        })) {
+          const fileBuffer = fs.readFileSync(`${pdfDistDir}/${fileName1}`);
+          const pdfDoc = await PDFDocument.load(new Uint8Array(fileBuffer));
+          const pagesCount = pdfDoc.getPageCount();
+
+          if (pagesCount > 4) {
+            logger.error(`${fileName1}: ${pagesCount} pages`);
+            hasOverprinting = true;
+          } else {
+            logger.info(`${fileName1}: ${pagesCount} pages`);
+          }
+        }
+
+        if (hasOverprinting) {
+          throw new Error(
+            'One of the flyers pdf had more than 4 pages. See log messages above.',
+          );
+        }
+        logger.info('No overprinting detected.');
       },
     },
   };
