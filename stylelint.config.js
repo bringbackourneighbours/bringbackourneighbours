77bacdb1371f82dfc44d9688b7adbd131a1afd9c
Johannes Voigt <10059188+jvoigt@users.noreply.github.com>
2025-07-13 18:12:16 +0200

    ci(#442): stylelint with artifacts

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,7 @@
 /** @type {import('stylelint').Config} */
 export default {
   extends: ['stylelint-config-standard', 'stylelint-config-html/astro'],
+  ignoreFiles: ['dist/**'],
   rules: {
     'no-empty-source': null, // astro file without style would break this rule
     'selector-class-pattern': [
