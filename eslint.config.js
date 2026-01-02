85020b1114a08006e59b8c0a556815f6ab0689d6
Johannes Voigt <10059188+jvoigt@users.noreply.github.com>
2026-01-02 17:57:38 +0100

    chore(#1000): reuse ignored files with eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,14 +2,17 @@ import eslintPluginAstro from 'eslint-plugin-astro';
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import { includeIgnoreFile } from '@eslint/compat';
+import { fileURLToPath } from 'node:url';
+
+// eslint-disable-next-line no-undef
+const gitignorePath = fileURLToPath(new URL('.gitignore', import.meta.url));
 
 export default [
+  includeIgnoreFile(gitignorePath, 'Imported .gitignore patterns'),
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   ...eslintPluginAstro.configs['flat/recommended'],
   ...eslintPluginAstro.configs['flat/jsx-a11y-strict'],
   eslintConfigPrettier,
-  {
-    ignores: ['.astro/**'],
-  },
 ];
