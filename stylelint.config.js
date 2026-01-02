a8ec1f5e0f85d0a8fcd8d2737f3acffcbb06c501
Johannes Voigt <10059188+jvoigt@users.noreply.github.com>
2026-01-02 17:28:03 +0100

    chore(#1000): reuse ignored files with stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,9 +1,7 @@
 /** @type {import('stylelint').Config} */
 export default {
   extends: ['stylelint-config-standard', 'stylelint-config-html/astro'],
-  ignoreFiles: ['dist/**'],
   rules: {
-    'no-empty-source': null, // astro file without style would break this rule
     'selector-class-pattern': [
       '^(-)?([a-z][a-z0-9]*)(-[a-z0-9]+)*$',
       {
