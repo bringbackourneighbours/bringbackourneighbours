ef87e3ec4fa01a5a04afe589b1d9ed186ab25980
Marcel <gitea@endeavr.de>
2026-01-24 14:37:28 +0100

    remove spaces

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { defineMiddleware } from 'astro:middleware';
 
 import { DEFAULT_LANG, type LanguagesValue } from './model/languages.ts';
 // import AstroConfig from '../astro.config.mjs';  -> old import. new import WIP
-import { base } from 'astro:config/server'
+import { base } from 'astro:config/client';
 
 // `context` and `next` are automatically typed
 export const onRequest = defineMiddleware((context, next) => {
@@ -13,17 +13,11 @@ export const onRequest = defineMiddleware((context, next) => {
 
   context.locals.isKit =
     context.url.pathname.startsWith(`${base}/internal-print/kit`) ||
-    context.url.pathname.startsWith(
-      `${base}/${context.params['lang']}/kit/`,
-    );
+    context.url.pathname.startsWith(`${base}/${context.params['lang']}/kit/`);
 
   context.locals.isFlyer =
-    context.url.pathname.startsWith(
-      `${base}/internal-print/flyer`,
-    ) ||
-    context.url.pathname.startsWith(
-      `${base}/${context.params['lang']}/flyer/`,
-    );
+    context.url.pathname.startsWith(`${base}/internal-print/flyer`) ||
+    context.url.pathname.startsWith(`${base}/${context.params['lang']}/flyer/`);
 
   // Read Lang (or default) from params
   context.locals.lang =
