f090f8885e517b73b9ef07a976b4fcd566649a39
Marcel Nowicki <94857567+blindi0815@users.noreply.github.com>
2026-01-24 14:27:42 +0100

    Refactor middleware to use base from astro config
    use new virtual module feature

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,28 @@
 import { defineMiddleware } from 'astro:middleware';
 
 import { DEFAULT_LANG, type LanguagesValue } from './model/languages.ts';
-import AstroConfig from '../astro.config.mjs';
+// import AstroConfig from '../astro.config.mjs';  -> old import. new import WIP
+import { base } from 'astro:config/server'
 
 // `context` and `next` are automatically typed
 export const onRequest = defineMiddleware((context, next) => {
   // Components can check if isPrint and show things differently
   context.locals.isPrint = context.url.pathname.startsWith(
-    `${AstroConfig.base}/internal-print/`,
+    `${base}/internal-print/`,
   );
 
   context.locals.isKit =
-    context.url.pathname.startsWith(`${AstroConfig.base}/internal-print/kit`) ||
+    context.url.pathname.startsWith(`${base}/internal-print/kit`) ||
     context.url.pathname.startsWith(
-      `${AstroConfig.base}/${context.params['lang']}/kit/`,
+      `${base}/${context.params['lang']}/kit/`,
     );
 
   context.locals.isFlyer =
     context.url.pathname.startsWith(
-      `${AstroConfig.base}/internal-print/flyer`,
+      `${base}/internal-print/flyer`,
     ) ||
     context.url.pathname.startsWith(
-      `${AstroConfig.base}/${context.params['lang']}/flyer/`,
+      `${base}/${context.params['lang']}/flyer/`,
     );
 
   // Read Lang (or default) from params
