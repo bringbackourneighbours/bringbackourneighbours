77bbb9343ce4312fbed9edb6be44644b79696f04
jvoigt <10059188+jvoigt@users.noreply.github.com>
2026-02-06 15:41:18 +0000

    chore(#1063): set vite server.host to '127.0.0.1' for compatibility with containers

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,15 +6,19 @@ import layoutFlyers from './src/integrations/layout-flyers';
 
 const isDev = import.meta.env.DEV;
 const isCodespace = import.meta.env.CODESPACES;
+
 const codespaceUrl = `https://${import.meta.env.CODESPACE_NAME}.${import.meta.env.GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}/`;
 const localhostUrl = 'http://localhost:4321/';
+const prodUrl = 'https://bringbackourneighbours.de/';
+const prodLinkUrl = 'bbonlink.de';
+const basePath = '';
+// TODO: i think the basepath should be '/' so we could omit it above
 
 const devUrl = isCodespace ? codespaceUrl : localhostUrl;
-const siteUrl = isDev ? devUrl : 'https://bringbackourneighbours.de/';
-const basePath = '';
+const siteUrl = isDev ? devUrl : prodUrl;
 
 export const previewUrl = devUrl;
-export const linkUrl = isDev ? `${devUrl}link` : 'bbonlink.de';
+export const linkUrl = isDev ? `${devUrl}link` : prodLinkUrl;
 
 // https://astro.build/config
 export default defineConfig({
@@ -23,5 +27,8 @@ export default defineConfig({
   integrations: [mdx(), printPdfs(), checkFlyers(), layoutFlyers()],
   build: {
     concurrency: 4,
+  },
+  server: {
+    host: '127.0.0.1',
   },
 });
