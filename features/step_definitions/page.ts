7f6eeea4d37cd2cb498f322827d99e20a6f426cd
Johannes Voigt <10059188+jvoigt@users.noreply.github.com>
2026-08-13 19:52:09 +0200

    feat: test with axe for a11y violations

--- features/step_definitions/page.ts
+++ features/step_definitions/page.ts
@@ -2,6 +2,9 @@ import { Then } from '@cucumber/cucumber';
 import type { PlaywrightWorld } from '../support/world.ts';
 import { expect } from '@playwright/test';
 
+import AxeBuilder from '@axe-core/playwright';
+import type { AxeResults } from 'axe-core';
+
 Then(
   'i see a title containing {string}',
   async function (this: PlaywrightWorld, expectedTitle: RegExp) {
@@ -14,5 +17,33 @@ Then(
   async function (this: PlaywrightWorld, expectedUrl: RegExp) {
     await this.screen.page.waitForURL(new RegExp(expectedUrl));
     expect(this.screen.page.url()).toMatch(new RegExp(expectedUrl));
+  },
+);
+
+Then(
+  'there are no accessibility violations except {string}',
+  async function (this: PlaywrightWorld, violations: string) {
+    function violationRulesNames(
+      accessibilityScanResults: AxeResults,
+    ): string[] {
+      return accessibilityScanResults.violations
+        .map((violation) => violation.id)
+        .sort();
+    }
+    function extractExpectedViolations(violations: string) {
+      return violations
+        .split(',')
+        .map((v) => v.trim())
+        .sort();
+    }
+
+    const accessibilityScanResults = await new AxeBuilder({
+      page: this.screen.page,
+    }).analyze();
+
+    const expectedViolationList = extractExpectedViolations(violations);
+    const actualViolations = violationRulesNames(accessibilityScanResults);
+
+    expect(actualViolations).toEqual(expectedViolationList);
   },
 );
