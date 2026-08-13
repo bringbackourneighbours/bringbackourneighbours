e8fdf5d1b976d4800899b111aa17109114e3f231
Johannes Voigt <10059188+jvoigt@users.noreply.github.com>
2026-08-13 19:02:44 +0200

    chore(deps): remove eslint-plugin-jsx-a11y

--- eslint.config.js
+++ eslint.config.js
@@ -13,6 +13,5 @@ export default [
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   ...eslintPluginAstro.configs['flat/recommended'],
-  ...eslintPluginAstro.configs['flat/jsx-a11y-strict'],
   eslintConfigPrettier,
 ];
