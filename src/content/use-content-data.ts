4012ef17d5540ce1ed1afab8dbc783b91993d53b
Johannes Voigt <10059188+jvoigt@users.noreply.github.com>
2026-09-26 16:53:59 +0200

    chore(#1217): supress warnings when Block not found for unsupported language

--- src/content/use-content-data.ts
+++ src/content/use-content-data.ts
@@ -1,4 +1,4 @@
-import { getEntry, type InferEntrySchema } from 'astro:content';
+import { getCollection, getEntry, type InferEntrySchema } from 'astro:content';
 
 import { Languages, type LanguagesValue } from '../model/languages';
 
@@ -8,8 +8,15 @@ export const useContentData = async <T extends MarkdownCollections>(
   collection: T,
   identifier: string,
   language: LanguagesValue | Languages,
+  suppressWarning = false,
 ): Promise<InferEntrySchema<T> | undefined> => {
-  const foundEntry = await getEntry(collection, `${identifier}/${language}`);
-
-  return foundEntry?.data as InferEntrySchema<T> | undefined;
+  if (suppressWarning) {
+    const foundEntries = await getCollection(collection, (item) => {
+      return item.id === `${identifier}/${language}`;
+    });
+    return foundEntries[0]?.data as InferEntrySchema<T> | undefined;
+  } else {
+    const foundEntry = await getEntry(collection, `${identifier}/${language}`);
+    return foundEntry?.data as InferEntrySchema<T> | undefined;
+  }
 };
